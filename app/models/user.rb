6cbc5a3f6afdac7489b8258715f71f0498cbe9d7
Max Shelley <max@measuredbrilliance.com>
2014-02-22 17:07:48 +0000

    Added a full name method to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
