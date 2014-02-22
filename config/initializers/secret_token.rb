e21c532d8196a9c2d457e1d1268df7d0be7ad739
Max Shelley <max@measuredbrilliance.com>
2014-02-22 13:21:14 +0000

    Initial commit. Blank Rails installation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialimpactstories::Application.config.secret_key_base = '4bd91622fe522b6e105117750110bc8e402d2c1bbcc62a165a891b49a33888ca6e46ef8180a284c1c0d4e421db73248c6ea8df7898a3149ce309b16e947a6d66'
