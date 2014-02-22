0cdb25bcff8b046b11662a9ea2a854e356e70dbc
Max Shelley <max@measuredbrilliance.com>
2014-02-22 15:02:33 +0000

    Added scaffold for a Story.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Socialimpactstories::Application.routes.draw do
+  resources :stories
+
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
